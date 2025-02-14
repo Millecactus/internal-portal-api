e066337111c55bcd60cab884a8b827a7e9122dba
Florian Duport <florianduport@mac.lan>
2025-02-14 15:53:35 +0100

    update presence post data

--- modules/presence/routes/presence.router.js
+++ modules/presence/routes/presence.router.js
@@ -2,6 +2,7 @@ import routerBase from 'endurance-core/lib/router.js';
 import { emitter, eventTypes } from 'endurance-core/lib/emitter.js';
 import Presence from '../models/presence.model.js';
 import moment from 'moment';
+import { auth, accessControl } from 'endurance-core/lib/auth.js';
 
 const router = routerBase({requireDb: true});
 
@@ -28,6 +29,34 @@ router.get("/weekly-presence", async (req, res) => {
         res.status(500).send('Internal Server Error');
     }
 });
-router.autoWire(Presence, 'Presence', {});
+
+router.post("/presence", accessControl.isAuthenticated(), async (req, res) => {
+    try {
+         const userId = req.user.id; // Supposons que l'ID de l'utilisateur est maintenant extrait de l'accessToken
+        const { date, type } = req.body;
+
+        if (!date || !type) {
+            return res.status(400).send('La date et le type sont requis');
+        }
+
+        if (!['office', 'client', 'remote', 'school', 'away'].includes(type)) {
+            return res.status(400).send('Type de présence invalide');
+        }
+
+        const newPresence = new Presence({
+            user: userId,
+            date: moment(date).toDate(),
+            type: type
+        });
+
+        await newPresence.save();
+        res.status(201).json(newPresence);
+    } catch (error) {
+        console.error('Erreur lors de la création d\'une nouvelle entrée de présence:', error);
+        res.status(500).send('Erreur interne du serveur');
+    }
+});
+
+
 
 export default router;
