ba69c8a9f9f1989353fae674d6453898dfb8b6f4
Florian Duport <florianduport@mac.lan>
2025-03-12 09:18:56 +0100

    fix lootbox cheat

--- modules/levelling/routes/daily-lootbox.router.js
+++ modules/levelling/routes/daily-lootbox.router.js
@@ -4,7 +4,7 @@ import { emitter, eventTypes } from 'endurance-core/dist/emitter.js';
 const router = routerBase();
 
 router.get("/", async (req, res) => {
-  emitter.emit(eventTypes.GENERATE_DAILY_LOOTBOX);
+  emitter.emit(eventTypes.GENERATE_DAILY_LOOTBOX_V2);
   res.send('ok');
 });
 
